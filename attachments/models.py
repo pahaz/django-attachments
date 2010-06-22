004d9e6d8e6b0965892a832a43e9f04ce814a396
Jason Mayfield <jwmayfield@mini.local>
2010-06-22 19:49:07 -0400

    don't want attachments in media folder - will eventually use a setting to determine root

--- attachments/models.py
+++ attachments/models.py
@@ -15,7 +15,7 @@ class AttachmentManager(models.Manager):
 class Attachment(models.Model):
     def attachment_upload(instance, filename):
         """Stores the attachment in a "per module/appname/primary key" folder"""
-        return 'attachments/%s/%s/%s' % (
+        return '../uploads/attachments/%s/%s/%s' % (
             '%s_%s' % (instance.content_object._meta.app_label,
                        instance.content_object._meta.object_name.lower()),
                        instance.content_object.pk,
