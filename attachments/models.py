66317e6791f373ae5b326e617a4366dca57c5790
Jason Mayfield <jwmayfield@mini.local>
2010-06-23 14:58:07 -0400

    add django setting for base upload directory

--- attachments/models.py
+++ attachments/models.py
@@ -1,10 +1,16 @@
-from datetime import datetime
-import os
-from django.db import models
+from os import path as os_path
+
+from django.conf import settings
 from django.contrib.auth.models import User
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
+from django.core.files.storage import FileSystemStorage
+from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+
+fs = FileSystemStorage(location=settings.ATTACHMENTS_STORAGE_ROOT)
+
 
 class AttachmentManager(models.Manager):
     def attachments_for_object(self, obj):
@@ -12,10 +18,11 @@ class AttachmentManager(models.Manager):
         return self.filter(content_type__pk=object_type.id,
                            object_id=obj.id)
 
+
 class Attachment(models.Model):
     def attachment_upload(instance, filename):
         """Stores the attachment in a "per module/appname/primary key" folder"""
-        return '../uploads/attachments/%s/%s/%s' % (
+        return 'attachments/%s/%s/%s' % (
             '%s_%s' % (instance.content_object._meta.app_label,
                        instance.content_object._meta.object_name.lower()),
                        instance.content_object.pk,
@@ -26,8 +33,10 @@ class Attachment(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     content_object = generic.GenericForeignKey('content_type', 'object_id')
-    creator = models.ForeignKey(User, related_name="created_attachments", verbose_name=_('creator'))
-    attachment_file = models.FileField(_('attachment'), upload_to=attachment_upload)
+    creator = models.ForeignKey(User, related_name="created_attachments",
+            verbose_name=_('creator'))
+    attachment_file = models.FileField(_('attachment'), storage=fs,
+            upload_to=attachment_upload)
     created = models.DateTimeField(_('created'), auto_now_add=True)
     modified = models.DateTimeField(_('modified'), auto_now=True)
     description = models.TextField(_('description'), blank=True)
@@ -44,4 +53,4 @@ class Attachment(models.Model):
 
     @property
     def filename(self):
-        return os.path.split(self.attachment_file.name)[1]
+        return os_path.split(self.attachment_file.name)[1]
